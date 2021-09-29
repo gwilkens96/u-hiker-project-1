432a297240dce750a64dfa663966ff7aced4bbdb
Noah Schmidt <noahsmith37@gmail.com>
2021-09-29 11:14:37 -0400

    Added a very basic skeleton for JS

---
+++ assets/js/results.js
@@ -0,0 +1,7 @@
+//Javascript file for results page to display the resulting route
+
+//Button to "Plan a new trip"
+
+//Functions to get Starting and Ending Locations
+
+//Functions to display the route directions
