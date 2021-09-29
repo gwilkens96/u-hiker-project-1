588861a637fbb7218fbb2fe1d80aaac92e5ecd32
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-09-29 12:13:02 -0400

    added html ids and defined them in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
 //get elements by id
-
+var searchForm = $('#searchForm');
+var search_input = $('#search_input');
+var search_results = $('#search_results');
 //function to call location data
 //prevent default
-function callData () {
+function callData (event) {
+    event.preventDefault();
+    
 
 }
 
@@ -17,6 +21,7 @@ function displayData() {
 }
 
 // add event listener for button click
+
 
 
 //button should display local parks in the area
