7c4ba63dbedc08677b3f841e8c77e2dc2ebbfefd
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-09-29 14:18:52 -0400

    fixed duplicate tags in html and added search functions in js

--- assets/js/results.js
+++ assets/js/results.js
@@ -20,5 +20,37 @@ function planButton(){
 //Get destination location from the index and display in box
 
 //Accept a starting location for directions between start/end
+// JavaScript code
+function search_origin() {
+	let input = document.getElementById('originFormInput').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('origin');
+	
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
+}
+
+function search_destination() {
+	let input = document.getElementById('destinationFormInput').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('destination');
+	
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
+}
+
 
 //Functions to display the route directions from api
+let app = document.querySelector('#directions');
